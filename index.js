b309096f65ff0eb7fac199019d6ec490215c28c6
Franco <frfantusatti@protonmail.com>
2025-02-11 21:35:43 -0300

    add json server

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const jsonServer = require('json-server');
 
-app.use(cors())
+const app = express();
+const port = process.env.PORT || 3000; // Render usa process.env.PORT
+
+app.use(cors());
 app.use(express.json());
 
-require('dotenv').config();
-
-const port = process.env.PORT || 3000;
-
-const postsRoutes = require('./routes/route')
-const getData = require('./routes/data')
+// Cargar rutas
+const postsRoutes = require('./routes/posts');
+const getData = require('./routes/data');
 
 app.use('/api/data', getData);
 app.use('/api', postsRoutes);
 
+// Configurar JSON Server (Para db.json)
+const dbRouter = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+app.use('/json', middlewares, dbRouter);
+
+// Iniciar servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
